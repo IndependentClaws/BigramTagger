b59e160a2a37197a670de4bc699939eee5c68ce4
IndependentClaws <independentclaws.nlp@gmail.com>
2019-09-16 07:24:10 -0400

    Initial Commit

---
+++ BigramTagger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BigramTagger
+//
+//  Created by Alex Makedonski on 9/16/19.
+//  Copyright © 2019 Alex Makedonski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
